ad1f7acd4252c8b0a14030b8f118b3e5a6fae2ff
AnanduMohan103 <anandhumohan103@gmail.com>
2022-10-14 01:11:58 +0530

    new

--- src/app/addemployee/addemployee.component.ts
+++ src/app/addemployee/addemployee.component.ts
@@ -8,6 +8,39 @@ import { Component, OnInit } from '@angular/core';
 export class AddemployeeComponent implements OnInit {
 
   constructor() { }
+  code=""
+  name=""
+  designation=""
+  gender=""
+  comName=""
+  salary=""
+  comAddress=""
+  mobile=""
+  email=""
+  experience=""
+  bloodGroup=""
+  dob=""
+  
+
+readValues=()=>{
+  let data={ 
+    "code":this.code,
+  "name":this.name,
+  "designation":this.designation,
+  "gender":this.gender,
+  "comName":this.comName,
+  "salary":this.salary,
+  "comAddress":this.comAddress,
+  "mobile":this.mobile,
+  "email":this.email,
+  "experience":this.experience,
+  "bloodGroup":this.bloodGroup,
+  "dob":this.dob
+  }
+  console.log(data)
+}
+
+
 
   ngOnInit(): void {
   }
